dc9434feb5186b49ae1e3c2c7e359f41ea6140be
Akshit Chibber <akshitchibber11@gmail.com>
2026-02-02 23:03:04 +0530

    Day001: Array based questions

---
+++ Day001/Day001ques1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+
+    scanf("%d", &n);
+
+    int arr[100];   // extra space for insertion
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    // shift elements to right
+    for(int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // insert element
+    arr[pos - 1] = x;
+
+    // print updated array
+    for(int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
